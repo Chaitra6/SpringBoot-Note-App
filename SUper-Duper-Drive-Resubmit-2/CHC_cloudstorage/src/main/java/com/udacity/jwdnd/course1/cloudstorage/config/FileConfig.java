23a111c494c706a756bfebe856fd89aad0f5a50e
Chaitra H C <6chaitra8@gmail.com>
2022-05-31 16:51:53 +0530

    Add files via upload

---
+++ SUper-Duper-Drive-Resubmit-2/CHC_cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/FileConfig.java
@@ -0,0 +1,21 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+//File Configuration to upload files
+@Configuration
+public class FileConfig {
+
+    private String upload_Directory = "./uploads/" ;
+
+    public void setUpload_Directory(String uDir){
+        this.upload_Directory = uDir;
+    }
+
+    public String getUpload_Directory(){
+        return upload_Directory;
+    }
+}
